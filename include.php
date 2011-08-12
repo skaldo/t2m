92e31172d2bab6d735214066b6251113b1d25a47
evandar <tomaspecka@gmail.com>
2011-08-12 16:11:10 +0200

    more cleanup & tweaks

--- include.php
+++ include.php
@@ -34,7 +34,7 @@ $errorOutput = array(
 define("ERROR_FATAL", "Chyba:", TRUE);
 define("ERROR_WARNING", "Upozornění:", TRUE);
 define("ERROR_TIP", "Tip:", TRUE);
-define("ERROR_INPUT_ERROR_NEAR", "Chyba je poblíž: ", TRUE);
+define("ERROR_MORE_ERROR_NEAR", "Chyba je poblíž: ", TRUE);
 define("ERROR_INPUT_SIZE_EXCEEDED", "Překročili jste maximální velikost vstupu ", TRUE);
 define("ERROR_INPUT_EMPTY", "Nezapomněli jste na něco?", TRUE);
 define("ERROR_UNHANDLED", "Please remember what you were doint and contact administrator<br />To continue reload page or press F5.", TRUE);
@@ -44,8 +44,8 @@ define("ERROR_MORSE_TIP_LIST", "Máte problémy s Morseovou abecedou? Podívejte
 
 function diacriticFree($text) {
     $array = Array('ä' => 'a', 'Ä' => 'A', 'á' => 'a', 'Á' => 'A', 'à' => 'a', 'À' => 'A', 'ã' => 'a', 'Ã' => 'A', 'â' => 'a', 'Â' => 'A', 'č' => 'c', 'Č' => 'C', 'ć' => 'c', 'Ć' => 'C', 'ď' => 'd', 'Ď' => 'D', 'ě' => 'e', 'Ě' => 'E', 'é' => 'e', 'É' => 'E', 'ë' => 'e', 'Ë' => 'E', 'è' => 'e', 'È' => 'E', 'ê' => 'e', 'Ê' => 'E', 'í' => 'i', 'Í' => 'I', 'ï' => 'i', 'Ï' => 'I', 'ì' => 'i', 'Ì' => 'I', 'î' => 'i', 'Î' => 'I', 'ľ' => 'l', 'Ľ' => 'L', 'ĺ' => 'l', 'Ĺ' => 'L', 'ń' => 'n', 'Ń' => 'N', 'ň' => 'n', 'Ň' => 'N', 'ñ' => 'n', 'Ñ' => 'N', 'ó' => 'o', 'Ó' => 'O', 'ö' => 'o', 'Ö' => 'O', 'ô' => 'o', 'Ô' => 'O', 'ò' => 'o', 'Ò' => 'O', 'õ' => 'o', 'Õ' => 'O', 'ő' => 'o', 'Ő' => 'O', 'ř' => 'r', 'Ř' => 'R', 'ŕ' => 'r', 'Ŕ' => 'R', 'š' => 's', 'Š' => 'S', 'ś' => 's', 'Ś' => 'S', 'ť' => 't', 'Ť' => 'T', 'ú' => 'u', 'Ú' => 'U', 'ů' => 'u', 'Ů' => 'U', 'ü' => 'u', 'Ü' => 'U', 'ù' => 'u', 'Ù' => 'U', 'ũ' => 'u', 'Ũ' => 'U', 'û' => 'u', 'Û' => 'U', 'ý' => 'y', 'Ý' => 'Y', 'ž' => 'z', 'Ž' => 'Z', 'ź' => 'z', 'Ź' => 'Z');
-    $temp = strtr($text, $array);
-    return $temp;
+    $return = strtr($text, $array);
+    return $return;
 }
 
 function isError($type) {
@@ -122,21 +122,22 @@ function morseCode($input, $encode) {
     global $morse_buffer;
     $return = NULL;
     $i = 0;
-    
+
     if ($encode == TRUE) {
         //MORSECODE ENCODE
         $input = diacriticFree($input);
         $input = strtolower($input);
+        $input = preg_replace('!\s+!', ' ', $input); //multiple spaces into one space
 
         $getCh = strpos($input, "ch");  //do this before the string is splitted
-        $input = str_split($input); //the only exception
+        $input = str_split($input);
         //handling Ch character, skip if ch not found
         if ($getCh !== FALSE) {
-            for ($i = 0; $i < count($input) - 1; $i++) {
-                if (($input[$i] == "c") && ($input[$i + 1] == "h")) {
-                    $input[$i] = "ch";
+            for ($j = 0; $j < count($input) - 1; $j++) {
+                if (($input[$j] == "c") && ($input[$j + 1] == "h")) {
+                    $input[$j] = "ch";
                     //delete H and re-index array
-                    unset($input[$i + 1]);
+                    unset($input[$j + 1]);
                     $input = array_values($input);
                 }
             }
@@ -144,16 +145,15 @@ function morseCode($input, $encode) {
         unset($getCh);
 
         foreach ($input as $temp) {
-            $i = 0;
             //spaces need to have two slashes, make them without spaces - str_replace hack else return character in morseCode
             if (!array_key_exists($temp, $morse)) {
                 doError(ERROR_MORSE_T2M_INPUT, 1);
-                doError("<span class=\"error\">" . ERROR_INPUT_ERROR_NEAR . "</span>" . ($i + 1) . ". písmeno (Neznámý znak <span class=\"red\">" . $temp . "</span>)", 3);
+                doError("<span class=\"error\">" . ERROR_MORE_ERROR_NEAR . "</span>" . ($i + 1) . ". písmeno (Neznámý znak <span class=\"red\">" . $temp . "</span>)", 3);
                 doError(ERROR_MORSE_TIP_LIST, 0);
-                $return .= "<span class=\"red error\" title = '" . ERROR_INPUT_ERROR_NEAR . ($i + 1) . ". písmeno'>*</span> / ";
+                $return .= "<span class=\"red error\" title = '" . ERROR_MORE_ERROR_NEAR . ($i + 1) . ". písmeno (Neznámý znak: " . $temp . ")'>*</span> / ";
             } elseif ($temp == " ") {
                 $return .= "/ ";
-                $return = str_replace("/ /", "// ", $return); //formatting, easier than if next char is space do this....
+                $return = str_replace("/ /", "//", $return); //formatting, easier than if next char is space do this....
             } else {
                 $return.= $morse[$temp] . " / ";
             }
@@ -170,15 +170,12 @@ function morseCode($input, $encode) {
         }
 
         foreach ($input as $temp) {
-            //IF SPACE
-            if ($temp == "") {
-                $return .= " ";
-            } elseif ((!in_array($temp, $morse))) {
+            if ((!in_array($temp, $morse))) {
                 doError(ERROR_MORSE_M2T_UNRECOGNIZED, 1);
-                doError("<span class=\"error\">" . ERROR_INPUT_ERROR_NEAR . "</span>" . ($i + 1) . ". písmeno: (Neznámý vstup: <span class=\"red\">" . $temp . "</span>)", 3);
+                doError("<span class=\"error\">" . ERROR_MORE_ERROR_NEAR . "</span>" . ($i + 1) . ". písmeno: (Neznámý vstup: <span class=\"red\">" . $temp . "</span>)", 3);
                 doError(ERROR_MORSE_TIP_LIST, 0);
-                $return .= "<span class=\"red error\" title = '" . ERROR_INPUT_ERROR_NEAR . " " . ($i + 1) . ". pismeno'>*</span>";
-            } elseif (in_array($temp, $morse)) {
+                $return .= "<span class=\"red error\" title = '" . ERROR_MORE_ERROR_NEAR . " " . ($i + 1) . ". pismeno (Neznámý vstup: " . $temp . "'>*</span>";
+            } else {
                 $return.= array_search($temp, $morse);
             }
             $i++;
@@ -199,7 +196,7 @@ function showOutput($input) {
 
             if ($lengthDiff < 1024) {
                 $lengthDiff .= " bytes";
-            } elseif (($rozdil > 1024) && ($lengthDiff < 1048576)) {
+            } elseif (($lengthDiff > 1024) && ($lengthDiff < 1048576)) {
                 $lengthDiff = round($lengthDiff / 1024) . " kilobytes";
             } else {
                 $lengthDiff = round($lengthDiff / 1024 / 1024) . " megabytes";
@@ -212,7 +209,6 @@ function showOutput($input) {
     //Trim some characters like:
     $input = trim($input); //whitespaces at start and end
     $input = str_replace(array("\r\n", "\r", "\n"), ' ', $input); // newlines
-    
     //we don't want to continue if no input or fatal error already
     if ($input == NULL) {
         doError(ERROR_INPUT_EMPTY, 2);
